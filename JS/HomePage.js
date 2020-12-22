b76a2b58a60f691a96f16f80bea58d4ed96242ad
אור בן סימון <or21290@gmail.com>
2020-12-22 11:34:20 +0200

    home page

---
+++ JS/HomePage.js
@@ -0,0 +1,7 @@
+﻿function showDropDownMenu() {
+    var navElement = document.getElementById("dropMenu");
+    if (navElement.className === "topnav")
+        navElement.className += " responsive";
+    else
+        navElement.className = "topnav";
+};
